d36b404418dc32b8da0a23bdd5d0be31aa7353f9
ifto-luan <124843693+ifto-luan@users.noreply.github.com>
2023-02-12 16:29:45 -0300

    Finished and working

--- MatrixService.java
+++ MatrixService.java
@@ -1,59 +1,60 @@
-
 public class MatrixService {
 
-    // public static Integer det(int[][] m) {
+    public static int det(int[][] m) {
 
-    // }
-
-    public static int[][] minor(int[][] m, int column) {
-
-        int[][] minor = new int[m.length - 1][m[0].length - 1];
-
-        for (int i = 0; i < m.length; i++) {
-
-            if (i == 0) {
-                continue;
-            }
-
-            for (int j = 0; j < m[i].length; j++) {
-
-                if (j == column + 1) {
-                    continue;
-                }
-
-
-            }
+        if (m.length == 1 && m[0].length == 1) {
+            return m[0][0];
         }
 
-        return minor;
+        int sum = 0;
 
-    }
+        for (int i = 0; i < m[0].length; i++) {
 
-    public static void print(int[][] m) {
-
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < m.length; i++) {
-
-            sb.append("[");
-
-            for (int j = 0; j < m[0].length; j++) {
-
-                if (j == m[i].length - 1) {
-
-                    sb.append(m[i][j]);
-                    continue;
-                }
-                sb.append(m[i][j] + "  ");
-
-            }
-            
-            sb.append("]\n");
+            sum += m[0][i] * Math.pow((-1), (1 + (i+1))) * det(minor(m, i+1));
 
         }
 
-        System.out.println(sb.toString());
+        return sum;
 
     }
 
+    private static int[][] minor(int[][] m, int column) {
+
+        try {
+
+            if (m.length != m[0].length) {
+                throw new Exception("This is not a square matrix");
+            } else if (m.length == 1 && m[0].length == 1) {
+                return m;
+            }
+
+            int[][] minor = new int[m.length - 1][m[0].length - 1];
+    
+            for (int i = 0; i < m.length; i++) {
+    
+                if (i == 0) {
+                    continue;
+                }
+    
+                for (int j = 0; j < m[i].length; j++) {
+
+                    if (j == column - 1) {
+                        continue;
+
+                    } else if (j < column - 1) {
+                        minor[i - 1][j] = m[i][j];
+
+                    } else if (j > column - 1 && j < m[i].length) {
+                        minor[i - 1][j - 1] = m[i][j];
+                    }
+                } 
+            }
+
+            return minor;
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
