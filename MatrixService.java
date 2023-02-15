64f4194c38da35f7a135bb83d4ddeabff5f38329
ifto-luan <124843693+ifto-luan@users.noreply.github.com>
2023-02-14 21:33:29 -0300

    Adding some unimplemented methods

--- MatrixService.java
+++ MatrixService.java
@@ -1,49 +1,117 @@
+/*
+
+This is a class that has some of the operations that can be done with a matrix
+
+*/
+
 public class MatrixService {
 
-    public static int det(int[][] m) {
+    public static double[][] sum(double[][] a, double[][] b) {
 
-        if (m.length == 1 && m[0].length == 1) {
-            return m[0][0];
-        }
-
-        int sum = 0;
-
-        for (int i = 0; i < m[0].length; i++) {
-
-            sum += m[0][i] * Math.pow((-1), (1 + (i+1))) * det(minor(m, i+1));
-
-        }
-
-        return sum;
+        return a;
 
     }
 
-    private static int[][] minor(int[][] m, int column) {
+    public static double[][] scalarMultiplication(double[][] m, double k) {
+        return m;
+    }
 
-        try {
+    public static double[][] multiplication(double[][] a, double[][] b) {
 
-            if (m.length != m[0].length) {
-                throw new Exception("This is not a square matrix");
-            } else if (m.length == 1 && m[0].length == 1) {
-                return m;
+        return a;
+
+    }
+
+    public static double[][] transposition(double[][] m) {
+        return m;
+    }
+
+    // Generates the identity matrix of a given order
+    public static int[][] genereateIdentityMatrix(int order) {
+
+        int[][] identity = new int[order][order];
+
+		for (int i = 0; i < order; i++) {
+
+            for (int j = 0; j < order; j++) {
+
+                if (i == j) {
+                    identity[i][j] = 1;
+                }
+
+            }
+        }
+        
+        return identity;
+    }
+
+    // Checks if a given matrix is a singleton
+    public static boolean isSingleton(double[][] m) {
+        return m.length == 1 && m[0].length == 1;
+    }
+
+    // Checks if a given matrix is a square matrix
+    private static boolean isSquare(double[][] m) {
+
+        for (int i = 0; i < m.length; i++) {
+            if (m.length != m[i].length) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public static boolean isInvertible(double[][] m) {
+        return true;
+    }
+
+    public static double[][] inverse(double[][] m) {
+
+        return m;
+
+    }
+    
+    public static boolean isSymmetric(double[][] m) {
+        return false;
+    }
+
+    public static boolean isSkewSymmetric(double[][] m) {
+        return false;
+    }
+
+    public static boolean isOrthogonal(double[][] m) {
+        return false;
+    }
+
+    // Generates the minor of an element from the given matrix in row 1 and the given column1
+    public static double[][] minor(double[][] m, double column) {
+
+            if (isSingleton(m)) { // A singleton matrix element cannot have a minor
+                return null;
             }
 
-            int[][] minor = new int[m.length - 1][m[0].length - 1];
+            double[][] minor = new double[m.length - 1][m[0].length - 1];
     
             for (int i = 0; i < m.length; i++) {
     
-                if (i == 0) {
+                if (i == 0) { // Ignores the elements in the first row
                     continue;
                 }
     
                 for (int j = 0; j < m[i].length; j++) {
 
-                    if (j == column - 1) {
+                    if (j == column - 1) { // Ignores the elements in the given column
                         continue;
 
+                    // If the element is in the left side of the ignored column, the element will be in the same column
+                    // and in a row above
                     } else if (j < column - 1) {
-                        minor[i - 1][j] = m[i][j];
+                        minor[i - 1][j] = m[i][j]; 
 
+                    // If the element is in the right side of the ignored column, the element will be in a column before
+                    // and in a row above
                     } else if (j > column - 1 && j < m[i].length) {
                         minor[i - 1][j - 1] = m[i][j];
                     }
@@ -51,10 +119,33 @@ public class MatrixService {
             }
 
             return minor;
+    }
 
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+    // Calculates the cofactor of an element from the matrix m in row 1 and the given column
+    public static double cofactor(double[][] m, double column) {
+
+        return (double) Math.pow((-1), 1 + column) * det(minor(m, column));
+
+    }
+
+    // Calculates the determinant of a square matrix by using Laplace's expansion
+    public static double det(double[][] m) throws RuntimeException {
+
+        double sum = 0;
+
+        if (isSingleton(m)) { 
+            return m[0][0]; // The determinant of a singleton matrix is it's only element
+        } else if (!isSquare(m)) {
+            throw new RuntimeException("This is not a square matrix");
         }
-        return null;
+
+        for (int i = 0; i < m[0].length; i++) {
+
+            sum += m[0][i] * cofactor(m, i+1);
+            
+        }
+
+        return sum;
+
     }
 }
